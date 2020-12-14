2a90d72b2122edaa040cf9f73216147fd50009be
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-12-14 18:23:04 -0300

    feat: add project initial files

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express = require('express');
+import routes from './routes';
+
+let app = express();
+
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000")
+})
